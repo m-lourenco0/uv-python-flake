2c72c25e9ff4fce40f11ad50a4ab18860a105ed4
c7lo <mlourencosantos_@hotmail.com>
2025-02-24 10:33:26 -0300

    feat:templates: add default template for nix flake init

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
       let
         pkgs = import nixpkgs { inherit system; };
         pkgsUnstable = import nixpkgs-unstable { inherit system; };
-        # Combine library paths for LD_LIBRARY_PATH
         ldLibraryPath = pkgs.lib.makeLibraryPath (with pkgs; [
           stdenv.cc.cc  # libstdc++.so.6
           zlib          # libz.so.1
@@ -21,28 +20,20 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            python3       # Standard Python 3
-            # uv            # For package management
-            stdenv.cc.cc  # Provides libstdc++.so.6
-            zlib          # Provides libz.so.1
+            python3
+            stdenv.cc.cc
+            zlib
           ] ++ [ pkgsUnstable.uv ];
 
           shellHook = ''
-            # Define the library path
             export LD_LIBRARY_PATH="${ldLibraryPath}:$LD_LIBRARY_PATH"
-
-            # Set the virtual environment path
             VENV_DIR=".venv"
-
-            # Create or use the virtual environment
             if [ ! -d "$VENV_DIR" ]; then
               echo "Creating a new virtual environment with uv..."
               uv init || { echo "Failed to create uv project!"; exit 1; }
             else
               echo "Using existing virtual environment..."
             fi
-
-            # Activate the virtual environment
             echo "Python development environment ready!"
             echo "Use 'uv add <package>' to install Python packages in the uv project."
           '';
@@ -51,10 +42,9 @@
         # Add templates output for nix flake init
         templates = {
           default = {
-            description = "A python development environment with uv for NixOS";
-            path = ./.;
+            description = "A Python development environment with uv for NixOS";
+            path = ./.;  # Points to the current directory (root of the flake)
           };
         };
-    });
+      });
 }
-
